55537eea9fe6c51b7610002f1f39a1bc25e55b9f
sylar <sylar.huang@all-dream.com>
2020-04-01 17:23:21 +0800

    add unsupport tips

--- app/components/RecordingPreview/index.js
+++ app/components/RecordingPreview/index.js
@@ -9,6 +9,11 @@ export default class RecordingPreview extends Component<Props> {
   props: Props;
 
   render() {
-    return <div className="recording-preview" />;
+    return (
+      <div className="recording-preview">
+        <h1>Preview</h1>
+        <h2>Ops, unsupport now</h2>
+      </div>
+    );
   }
 }
